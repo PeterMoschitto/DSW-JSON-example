d9ce1b4a34f8ad633fabd240c93dffbf80e076e6
PeterMoschitto <102267066+PeterMoschitto@users.noreply.github.com>
2024-03-20 13:30:48 -0700

    Update word_analyzer.py

--- word_analyzer.py
+++ word_analyzer.py
@@ -1,25 +1,27 @@
 import json
 
 #load the data from the JSON file
-with open('words.json') as words:
-    word_data = json.load(words) 
+with open('words.json') as food:
+    food_data = json.load(food) 
 
-print(word_data) #word_data is a list of dictionaries
+print(food_data) #word_data is a list of dictionaries
+
+print()
 
 #access the first element in the list and then access the value for the key 'word'
-print(word_data[0]['word'])
+print(food_data[0]['food'])
 
-for data in word_data: #data is each dictionary in the word_data list
-    print(data)
-    print()#print a blank line
+# for data in word_data: #data is each dictionary in the word_data list
+#     print(data)
+#     print()#print a blank line
 
-"""
-for data in word_data: 
-    if data['part of speech'] == 'verb':
-        print(data['word'])
+# """
+# for data in word_data: 
+#     if data['part of speech'] == 'verb':
+#         print(data['word'])
         
-num_nouns = 0
-for data in word_data:
-    if data['part of speech'] == 'noun':
-        num_nouns = num_nouns + 1
-print(num_nouns)"""
+# num_nouns = 0
+# for data in word_data:
+#     if data['part of speech'] == 'noun':
+#         num_nouns = num_nouns + 1
+# print(num_nouns)"""
